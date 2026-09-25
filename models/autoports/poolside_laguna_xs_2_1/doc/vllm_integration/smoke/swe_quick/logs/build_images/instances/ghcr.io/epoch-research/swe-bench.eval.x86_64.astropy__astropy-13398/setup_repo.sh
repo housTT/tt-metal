428351def01667d540a98cddedc8b8e19016228f
Adam Housman <ahousman@tenstorrent.com>
2026-09-25 08:53:51 -0400

    wip: untracked models/autoports/poolside_laguna_xs_2_1 (files <= 500 KB; doc/ evidence over that size is in the HF offload archive), preserved 2026-09-25 before the machine is offloaded

---
+++ models/autoports/poolside_laguna_xs_2_1/doc/vllm_integration/smoke/swe_quick/logs/build_images/instances/ghcr.io/epoch-research/swe-bench.eval.x86_64.astropy__astropy-13398/setup_repo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euxo pipefail
+cd /testbed
+git reset --hard 6500928dc0e57be8f06d1162eacc3ba5e2eff692
+git remote remove origin
+source /opt/miniconda3/bin/activate
+conda activate testbed
+echo "Current environment: $CONDA_DEFAULT_ENV"
+sed -i 's/requires = \["setuptools",/requires = \["setuptools==68.0.0",/' pyproject.toml
+python -m pip install -e .[test] --verbose
---
+++ models/autoports/poolside_laguna_xs_2_1/doc/vllm_integration/smoke/swebench_toolcall_mit/logs/build_images/instances/ghcr.io/epoch-research/swe-bench.eval.x86_64.astropy__astropy-12907/setup_repo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euxo pipefail
+cd /testbed
+git reset --hard d16bfe05a744909de4b27f5875fe0d4ed41ce607
+git remote remove origin
+source /opt/miniconda3/bin/activate
+conda activate testbed
+echo "Current environment: $CONDA_DEFAULT_ENV"
+sed -i 's/requires = \["setuptools",/requires = \["setuptools==68.0.0",/' pyproject.toml
+python -m pip install -e .[test] --verbose
